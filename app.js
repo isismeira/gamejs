37019d3b7b2aa92c79e502ff9717a4b8c2153d03
isismeira <isismeira127@gmail.com>
2024-06-04 09:40:55 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+
+alert('Boas Vindas ao jogo do número secreto');
+
+let numeroSecreto = 4;
+
+let chute;
+
+let tentativas = 1;
+
+while ( chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    if(numeroSecreto == chute) {
+        alert(`Isso aí, você descobriu o número secreto (${numeroSecreto}) com ${tentativas} tentativas`);
+    }
+    else{
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        //tentativas = tentativas + 1
+        tentativas++;
+    }
+}
+if (tentativas > 1) {
+    alert(`Isso aí, você descobriu o número secreto (${numeroSecreto}) com ${tentativas} tentativas`);
+}
